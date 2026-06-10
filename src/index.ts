5e2b60e4c6380669658d6a01ce2a2122f21da686
Marin Orlic <marin.orlic@ericsson.com>
2026-06-10 15:02:40 +0200

    test rename element 2

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ customElements.define('hasl4o-departure-card', HASLDepartureCard)
 
 window.customCards = window.customCards || []
 window.customCards.push({
-    type: "hasl4-departure-card",
-    name: "HASL4 Departure card",
+    type: "hasl4o-departure-card",
+    name: "HASL4o Departure card",
     description: "Show departure times for SL Trafik",
 })
